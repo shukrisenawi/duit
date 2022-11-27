bbad02f91f4a8f65ebf47b5113443ee7fdacda62
ShukriSenawi <shukrisenawi@gmail.com>
2022-11-27 11:02:56 +0800

    Create Dropdown Search Provinsi

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:dropdown_search2/dropdown_search2.dart';
+import 'package:duit/app/modules/home/province_model.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -14,14 +18,43 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: ListView(padding: EdgeInsets.all(20), children: [
-        DropdownSearch<String>(
-            mode: Mode.DIALOG,
-            items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
-            label: "Menu mode",
-            hint: "country in menu mode",
-            onChanged: print,
-            showSearchBox: true,
-            selectedItem: "Brazil"),
+        DropdownSearch<Province>(
+          label: "Provinsi",
+          showSearchBox: true,
+          onFind: (filter) async {
+            Uri url = Uri.parse('https://api.rajaongkir.com/starter/province');
+            try {
+              final response = await http.get(url, headers: {
+                "key": "0ae702200724a396a933fa0ca4171a7e",
+              });
+
+              var data = jsonDecode(response.body) as Map<String, dynamic>;
+
+              var statusCode = data['rajaongkir']['status']['code'];
+              if (statusCode != 200) {
+                throw data['rajaongkir']['status']['description'];
+              }
+
+              var listAllProvince =
+                  data['rajaongkir']['results'] as List<dynamic>;
+              var models = Province.fromJsonList(listAllProvince);
+              return models;
+            } catch (err) {
+              return List<Province>.empty();
+            }
+          },
+          onChanged: (value) => print(value),
+          popupItemBuilder: (context, item, isSelected) {
+            return Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Text(
+                "${item.province}",
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          },
+          itemAsString: (item) => item!.province!,
+        ),
       ]),
     );
   }
